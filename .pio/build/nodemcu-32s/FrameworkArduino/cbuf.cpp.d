1040a2686b2b04f0c16f28b4eb69e58f39d46424
mugendii <mugiendi318@gmail.com>
2025-05-22 21:29:19 +0300

    final working

---
+++ .pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Mugi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Mugi/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
